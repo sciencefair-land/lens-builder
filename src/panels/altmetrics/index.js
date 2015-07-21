63a57977b4024de5ce8de002557ee03e5a04513c
Michael Aufreiter <michael@substance.io>
2015-07-21 22:43:21 +0200

    Update index.js

--- src/panels/altmetrics/index.js
+++ src/panels/altmetrics/index.js
@@ -4,7 +4,7 @@ var Panel = require('lens/reader').Panel;
 var AltmetricsController = require('./altmetrics_controller');
 
 var panel = new Panel({
-	name: "altmetrics",
+  name: "altmetrics",
   type: 'resource',
   title: 'Altmetrics',
   icon: 'fa-bar-chart-o',
