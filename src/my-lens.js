442271b6a503d0884c016095228bbb90612a43df
Richard Smith-Unna <richardsmith404@gmail.com>
2016-04-06 13:11:33 +0100

    ScienceFair first lens-bundle version

--- src/my-lens.js
+++ src/my-lens.js
@@ -2,26 +2,11 @@
 
 var Lens = require("lens/reader");
 var panels = Lens.getDefaultPanels();
-  
+
 // All available converters
 var LensConverter = require("lens/converter");
-var CustomConverter = require("./custom_converter");
+var SciFairEuPmcConverter = require("./scifair_eupmc_converter");
 var ElifeConverter = require("lens/converter/elife_converter");
-
-// Custom Panels
-// -------------------
-// 
-// The following lines enable the altmetrics panel
-// which can be considered a demo implementation for a custom
-// panel in Lens
-// 
-// Find the code in panels/altmetrics and use it as an inspiration
-// to build your own Lens panel
-
-var altmetricsPanel = require('./panels/altmetrics');
-
-// Insert altmetrics panel at next to last position
-panels.splice(-1, 0, altmetricsPanel);
 
 var LensApp = function(config) {
   Lens.call(this, config);
@@ -31,7 +16,7 @@ LensApp.Prototype = function() {
 
   // Custom converters
   // --------------
-  // 
+  //
   // Provides a sequence of converter instances
   // Converter.match will be called on each instance with the
   // XML document to processed. The one that returns true first
@@ -40,7 +25,7 @@ LensApp.Prototype = function() {
 
   this.getConverters = function(converterOptions) {
     return [
-      new CustomConverter(converterOptions),
+      new SciFairEuPmcConverter(converterOptions),
       new ElifeConverter(converterOptions),
       new LensConverter(converterOptions)
     ]
@@ -48,7 +33,7 @@ LensApp.Prototype = function() {
 
   // Custom panels
   // --------------
-  // 
+  //
 
   this.getPanels = function() {
     return panels.slice(0);
---
+++ src/scifair_eupmc_converter.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var util = require("lens/substance/util");
+var LensConverter = require('lens/converter');
+var path = require('path')
+
+var SciFairEuPmcConverter = function(options) {
+  LensConverter.call(this, options);
+};
+
+SciFairEuPmcConverter.Prototype = function() {
+
+  var __super__ = LensConverter.prototype;
+
+  this.test = function(xmlDoc, documentUrl) {
+    xmlDoc.documentUrl = documentUrl
+    return /PMC[0-9]+/.test(documentUrl)
+  };
+
+  // Resolve figure urls
+  // --------
+  //
+
+  this.enhanceFigure = function(state, node, element) {
+    var relative_dir = path.dirname(state.xmlDoc.documentUrl)
+    var graphic = element.querySelector("graphic");
+    var url = graphic.getAttribute("xlink:href");
+    url = [
+      url,
+      '.jpg'
+    ].join('');
+    node.url = path.join(relative_dir, 'figures', url);
+  };
+
+};
+
+SciFairEuPmcConverter.Prototype.prototype = LensConverter.prototype;
+SciFairEuPmcConverter.prototype = new SciFairEuPmcConverter.Prototype();
+SciFairEuPmcConverter.prototype.constructor = SciFairEuPmcConverter;
+
+module.exports = SciFairEuPmcConverter;
